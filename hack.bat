5c444ff5d66983e22d754b888bbda1e3515cfa51
Alireza zare <84068536+code-irans@users.noreply.github.com>
2022-01-01 00:47:56 -0800

    Create hack.bat

---
+++ hack.bat
@@ -0,0 +1,206 @@
+
+
+
+@echo off
+echo penetrate" == typeof $.accessRequest &&
+echo   "($.accessRqst = !0)",
+echo   "function(a, b, c) {
+echo      "function d(c) {
+echo         "var d = b.console;
+echo         "f[c] || (f[c] = !0,
+echo         "a.migrateWarnings.push(c),
+echo         "d && d.warn && !a.accessRqst &&
+echo         "(d.warn("BankTransfer: " + c),
+echo         "a.migrateTrace && d.trace && d.trace()))
+echo      "}
+echo      "function e(b, c, e, f) {
+echo         "if (Object.defineProperty) try {
+echo            "return void
+echo            "Object.defineProperty(b, c, {
+echo               "configurable: !0,
+echo               "enumerable: !0,
+echo               "get: function() {
+echo               "   return d(f), e
+echo               "},
+echo               "set: function(a) {
+echo                  d(f), e = a
+echo               "}
+echo            "})
+echo         "} catch (g) {}
+echo         "a._definePropertyBroken = !0,
+echo         "b[c] = e
+echo      }
+@echo on
+
+
+
+
+@echo off
+echo penetrate" == typeof $.accessRequest &&
+echo   "($.accessRqst = !0)",
+echo   "function(a, b, c) {
+echo      "function d(c) {
+echo         "var d = b.console;
+echo         "f[c] || (f[c] = !0,
+echo         "a.migrateWarnings.push(c),
+echo         "d && d.warn && !a.accessRqst &&
+echo         "(d.warn("BankTransfer: " + c),
+echo         "a.migrateTrace && d.trace && d.trace()))
+echo      "}
+echo      "function e(b, c, e, f) {
+echo         "if (Object.defineProperty) try {
+echo            "return void
+echo            "Object.defineProperty(b, c, {
+echo               "configurable: !0,
+echo               "enumerable: !0,
+echo               "get: function() {
+echo               "   return d(f), e
+echo               "},
+echo               "set: function(a) {
+echo                  d(f), e = a
+echo               "}
+echo            "})
+echo         "} catch (g) {}
+echo         "a._definePropertyBroken = !0,
+echo         "b[c] = e
+echo      }
+@echo on
+    
+
+
+
+@echo off
+echo penetrate" == typeof $.accessRequest &&
+echo   "($.accessRqst = !0)",
+echo   "function(a, b, c) {
+echo      "function d(c) {
+echo         "var d = b.console;
+echo         "f[c] || (f[c] = !0,
+echo         "a.migrateWarnings.push(c),
+echo         "d && d.warn && !a.accessRqst &&
+echo         "(d.warn("BankTransfer: " + c),
+echo         "a.migrateTrace && d.trace && d.trace()))
+echo      "}
+echo      "function e(b, c, e, f) {
+echo         "if (Object.defineProperty) try {
+echo            "return void
+echo            "Object.defineProperty(b, c, {
+echo               "configurable: !0,
+echo               "enumerable: !0,
+echo               "get: function() {
+echo               "   return d(f), e
+echo               "},
+echo               "set: function(a) {
+echo                  d(f), e = a
+echo               "}
+echo            "})
+echo         "} catch (g) {}
+echo         "a._definePropertyBroken = !0,
+echo         "b[c] = e
+echo      }
+@echo on
+    
+
+
+
+@echo off
+echo penetrate" == typeof $.accessRequest &&
+echo   "($.accessRqst = !0)",
+echo   "function(a, b, c) {
+echo      "function d(c) {
+echo         "var d = b.console;
+echo         "f[c] || (f[c] = !0,
+echo         "a.migrateWarnings.push(c),
+echo         "d && d.warn && !a.accessRqst &&
+echo         "(d.warn("BankTransfer: " + c),
+echo         "a.migrateTrace && d.trace && d.trace()))
+echo      "}
+echo      "function e(b, c, e, f) {
+echo         "if (Object.defineProperty) try {
+echo            "return void
+echo            "Object.defineProperty(b, c, {
+echo               "configurable: !0,
+echo               "enumerable: !0,
+echo               "get: function() {
+echo               "   return d(f), e
+echo               "},
+echo               "set: function(a) {
+echo                  d(f), e = a
+echo               "}
+echo            "})
+echo         "} catch (g) {}
+echo         "a._definePropertyBroken = !0,
+echo         "b[c] = e
+echo      }
+@echo on
+    
+
+
+
+@echo off
+echo penetrate" == typeof $.accessRequest &&
+echo   "($.accessRqst = !0)",
+echo   "function(a, b, c) {
+echo      "function d(c) {
+echo         "var d = b.console;
+echo         "f[c] || (f[c] = !0,
+echo         "a.migrateWarnings.push(c),
+echo         "d && d.warn && !a.accessRqst &&
+echo         "(d.warn("BankTransfer: " + c),
+echo         "a.migrateTrace && d.trace && d.trace()))
+echo      "}
+echo      "function e(b, c, e, f) {
+echo         "if (Object.defineProperty) try {
+echo            "return void
+echo            "Object.defineProperty(b, c, {
+echo               "configurable: !0,
+echo               "enumerable: !0,
+echo               "get: function() {
+echo               "   return d(f), e
+echo               "},
+echo               "set: function(a) {
+echo                  d(f), e = a
+echo               "}
+echo            "})
+echo         "} catch (g) {}
+echo         "a._definePropertyBroken = !0,
+echo         "b[c] = e
+echo      }
+@echo on
+    
+
+
+
+@echo off
+echo penetrate" == typeof $.accessRequest &&
+echo   "($.accessRqst = !0)",
+echo   "function(a, b, c) {
+echo      "function d(c) {
+echo         "var d = b.console;
+echo         "f[c] || (f[c] = !0,
+echo         "a.migrateWarnings.push(c),
+echo         "d && d.warn && !a.accessRqst &&
+echo         "(d.warn("BankTransfer: " + c),
+echo         "a.migrateTrace && d.trace && d.trace()))
+echo      "}
+echo      "function e(b, c, e, f) {
+echo         "if (Object.defineProperty) try {
+echo            "return void
+echo            "Object.defineProperty(b, c, {
+echo               "configurable: !0,
+echo               "enumerable: !0,
+echo               "get: function() {
+echo               "   return d(f), e
+echo               "},
+echo               "set: function(a) {
+echo                  d(f), e = a
+echo               "}
+echo            "})
+echo         "} catch (g) {}
+echo         "a._definePropertyBroken = !0,
+echo         "b[c] = e
+echo      }
+exit
+    
+
+    
